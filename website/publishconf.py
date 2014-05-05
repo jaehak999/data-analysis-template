7254237ca1345c01e982a4b935ed45b8983deccd
Jaehak Yu <jaehak999@berkeley.edu>
2014-05-05 12:05:51 -0700

    edit

--- website/publishconf.py
+++ website/publishconf.py
@@ -14,7 +14,7 @@ from pelicanconf import *
 # URL of the github repository website associated with the fork
 # of this repository  (Replace accordingly)
 ####################################################################
-SITEURL = 'http://jaehak999.github.io/data-analysis-template'
+SITEURL = 'http://jaehak999.github.io/redbear'
 
 ####################################################################
 
